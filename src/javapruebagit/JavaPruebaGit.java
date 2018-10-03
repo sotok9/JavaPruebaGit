6e5240dc21a657d94b058859a4aeeb169ab34bcb
sotok9 <43590747+sotok9@users.noreply.github.com>
2018-10-03 16:34:27 -0700

    Agregue un saludo

--- src/javapruebagit/JavaPruebaGit.java
+++ src/javapruebagit/JavaPruebaGit.java
@@ -5,6 +5,8 @@ public class JavaPruebaGit {
     public static void main(String[] args) {
         System.out.println("hola chicos Git");
         System.out.println("Cheuqen");
+        
+        System.out.println("Saludos dek johny");
     }
     
 }
